2589229e4b0045022b8aaccf1dce7310ea6ccf39
VandersonMendes <109023638+VandersonMendes@users.noreply.github.com>
2022-11-22 14:23:24 -0300

    atualização

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ function mostrar(){
     const texto = document.querySelector(".carta-p")
     const gato = document.querySelector(".gato-feio")
     const decl = document.querySelector(".decl")
+    const cont = document.querySelector(".card")
     texto.classList.toggle("ativar")
     gato.classList.toggle("ativar")
     decl.classList.toggle("ativar")
+    cont.classList.toggle("ativar")
 }
 button.addEventListener("click", mostrar)
 
