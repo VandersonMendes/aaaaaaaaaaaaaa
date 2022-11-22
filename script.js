ebb4b149ab29555605fcd4fd9bc802b7dccd790f
VandersonMendes <109023638+VandersonMendes@users.noreply.github.com>
2022-11-22 15:11:49 -0300

    jjjohiophioph

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const button = document.querySelector(".carta")
 function mostrar(){
     const texto = document.querySelector(".carta-p")
+    const texto1 = document.querySelector(".carta-p-2")
     const gato = document.querySelector(".gato-feio")
     const decl = document.querySelector(".decl")
     const cont = document.querySelector(".card")
     texto.classList.toggle("ativar")
+    texto1.classList.toggle("ativar")
     gato.classList.toggle("ativar")
     decl.classList.toggle("ativar")
     cont.classList.toggle("ativar")
